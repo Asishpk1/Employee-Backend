245df84ea8068bcde503027c5f3f74fed502a1db
Asishpk1 <asishpkrishna@gmail.com>
2025-10-25 12:32:37 +0530

    Update

--- Controllers/employeeController.js
+++ Controllers/employeeController.js
@@ -1,79 +1,85 @@
 const employees = require("../Models/employeeModel");
 
 
-exports.addEmployeeController = async (req,res) =>{
+exports.addEmployeeController = async (req, res) => {
     console.log("Inside addEmployeeController");
-    const {name,email,position,department,salary} = req.body
+    const { name, email, position, department, salary } = req.body
 
-    try{
-        const existingEmployee = await employees.findOne({email})
-        if(existingEmployee){
+    try {
+        const existingEmployee = await employees.findOne({ email })
+        if (existingEmployee) {
             res.status(406).json("Email already exists")
         }
-        else{
-            const newEmployee = new employees({name,email,position,department,salary})
+        else {
+            const newEmployee = new employees({ name, email, position, department, salary })
             await newEmployee.save()
             res.status(200).json(newEmployee)
             console.log(newEmployee);
-            
+
         }
     }
-    catch(err){
+    catch (err) {
         console.log(err);
         res.status(401).json(err)
-        
+
     }
-    
+
 }
 
-exports.getAllEmployeesController = async (req,res) =>{
+exports.getAllEmployeesController = async (req, res) => {
     console.log("Inside getAllEmployeesController");
-    const {search}= req.query
+    const { search } = req.query
 
-    try{
-        const allEmployees = await employees.find({name:{$regex:search,$options:"i"}})
+    try {
+        const allEmployees = await employees.find({
+            $or: [
+                { name: { $regex: search, $options: "i" } },
+                { position: { $regex: search, $options: "i" } },
+                { department: { $regex: search, $options: "i" } }
+            ]
+        })
         res.status(200).json(allEmployees)
         console.log(allEmployees);
-        
+
     }
-    catch(err){
+    catch (err) {
         res.status(401).json(err)
     }
-    
+
 }
 
-exports.editEmployeeController = async (req,res) =>{
+exports.editEmployeeController = async (req, res) => {
     console.log("Inside editEmployeeController");
-    const {id} = req.params
-    const{name,email,position,department,salary} = req.body
+    const { id } = req.params
+    const { name, email, position, department, salary } = req.body
 
-    try{
-        const updatedEmployee = await employees.findByIdAndUpdate({_id:id},{name,email,position,department,salary},{new:true})
+    try {
+        const updatedEmployee = await employees.findByIdAndUpdate({ _id: id }, { name, email, position, department, salary }, { new: true })
         await updatedEmployee.save()
         res.status(200).json(updatedEmployee)
         console.log(updatedEmployee);
-        
+
     }
-    catch(err){
+    catch (err) {
         res.status(401).json(err)
         console.log(err);
-        
+
     }
-    
+
 }
 
-exports.deleteEmployeeController = async (req,res) =>{
+exports.deleteEmployeeController = async (req, res) => {
     console.log("Inside deleteEmployeeController");
-    const {id} = req.params
+    const { id } = req.params
 
-    try{
-        const deletedProject = await employees.findByIdAndDelete({_id:id})
+    try {
+        const deletedProject = await employees.findByIdAndDelete({ _id: id })
         res.status(200).json(deletedProject)
     }
-    catch(err){
+    catch (err) {
         console.log(err);
         res.status(401).json(err)
-        
+
     }
-    
+
 }
